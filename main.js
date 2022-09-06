09318b2fb86c068645d4e3c1c050a8c2076e2175
Chandupa Gunawardana <chandupamalith123@gmail.com>
2022-09-06 22:46:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+/* NavBar Animation */
+
+var menuBtn = document.getElementById("menuBtn")
+var sideNav = document.getElementById("sideNav")
+var menu = document.getElementById("menu")
+
+menuBtn.onclick = function() {
+    if (sideNav.style.right == "-250px") {
+        sideNav.style.right = "0";
+    } else {
+        sideNav.style.right = "-250px";
+    }
+}
+
+/* Smooth Scroll Animation */
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 500,
+    speedAsDuration: true
+});
+
+/* Cursor Animation */
+
+const cursor = document.querySelector('.cursor ');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top:" + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
